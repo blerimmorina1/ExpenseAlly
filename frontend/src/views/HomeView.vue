dcae4defcdf1a0a2745e54b87535f58af58b79a2
blerimmorina1 <b.morina93@gmail.com>
2024-12-15 22:04:50 +0100

    update App.vue and add header/footer

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="p-4">
+
+  </div>
 </template>
+
+<style scoped>
+
+</style>
